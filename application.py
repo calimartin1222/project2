bece617d08f3751baa19fa03e4ee4ebbaef0e996
Cali-Ann Sara Martin <calimartin1222@gmail.com>
2018-07-17 02:40:12 +0000

    got global channel list working, added vote source code

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, sessions, render_template
+from flask import Flask, sessions, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -8,17 +8,25 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 # list of all channels
+votes = {"yes": 0, "no": 0, "maybe": 0}
 channel_list = ['general']
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    global channel_list
+    return render_template("index.html", channel_list=channel_list, votes=votes)
 
 @app.route("/hello", methods=["POST"])
 def hello():
     greeting = request.form.get("greeting")
 
 @app.route("/newChannel", methods=["POST"])
-def hello():
+def newChannel():
     global channel_list
-    channel_list.apped(request.form.get("newChannel"))
+    channel_list.append(request.form.get("channelName"))
+
+@socketio.on("submit vote")
+def vote(data):
+    selection = data["selection"]
+    votes[selection] += 1
+    emit("vote totals", votes, broadcast=True)
--- static/index.js
+++ static/index.js
@@ -1,6 +1,7 @@
 if (!localStorage.getItem('name'))
                 localStorage.setItem('name', 'guest');
 document.addEventListener('DOMContentLoaded', () => {
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     document.getElementById("submit").disabled = true;
     var name = localStorage.getItem('name');
     if(name === "guest"){
@@ -8,8 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     const greeting = `Hello, ${name}!`;
     document.querySelector('#greeting').innerHTML = greeting;
-
-
     document.querySelector('#nameInput').onsubmit = () => {
         const request = new XMLHttpRequest();
         const name = document.querySelector('#name').value;
@@ -26,16 +25,35 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     document.querySelector('#newChannel').onsubmit = () => {
         const request = new XMLHttpRequest();
-        const newChannel = document.querySelector('#channelName').value;
+        const channelName = document.querySelector('#channelName').value;
         request.open('POST', '/newChannel');
         request.onload = () => {
             const li = document.createElement('li');
-            li.innerHTML = newChannel;
-            document.querySelector('#channelName').append(li);
+            li.innerHTML = channelName;
+            document.querySelector('#channels').append(li);
         };
         const data = new FormData();
-        data.append('newChannel', newChannel);
+        data.append('channelName', channelName);
         request.send(data);
         return false;
     };
+
+    socket.on('connect', () => {
+
+        // Each button should emit a "submit vote" event
+        document.querySelectorAll('button').forEach(button => {
+            button.onclick = () => {
+                const selection = document.querySelector('#chatInput').value;
+                socket.emit('submit vote', {'selection': selection});
+            };
+        });
+    });
+
+    // When a new vote is announced, increase the count
+    socket.on('vote totals', data => {
+        document.querySelector('#yes').innerHTML = data.yes;
+        document.querySelector('#no').innerHTML = data.no;
+        document.querySelector('#maybe').innerHTML = data.maybe;
+    });
+
 });
