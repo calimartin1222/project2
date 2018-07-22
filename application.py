655c362eeef4a956e7239c47d0cda1b0bae38cd2
Cali-Ann Sara Martin <calimartin1222@gmail.com>
2018-07-22 00:21:43 +0000

    added variable making divs with channels, messaging not working

--- application.py
+++ application.py
@@ -14,25 +14,26 @@ messages = []
 
 @app.route("/")
 def index():
-    return render_template("index.html", channel_list=channel_list, messages=messages)
+    return render_template("index.html", channel_list=channel_list, messages=messages, currentChannel=currentChannel)
 
 @app.route("/hello", methods=["POST"])
 def hello():
     greeting = request.form.get("greeting")
-    return greeting;
+    return greeting
 
 @app.route("/newChannel", methods=["POST"])
 def newChannel():
     channelName = request.form.get("channelName")
     channel_list.append(channelName)
-    return channelName;
+    return channelName
 
 @app.route("/<channel>", methods=["POST"])
 def channel(channel):
-    currentChannel=channel;
-    return channel;
+    currentChannel=channel
+    return channel
 
 @socketio.on("submit message")
 def submit(message):
-    messages.append(message)
-    emit("display messages", message, broadcast=True)
+    messageSend = f"user says: {message}"
+    messages.append(messageSend)
+    emit("display messages", messageSend, broadcast=True)
--- static/index.js
+++ static/index.js
@@ -1,30 +1,36 @@
 if (!localStorage.getItem('name'))
-
-                localStorage.setItem('name', 'guest');
+    localStorage.setItem('name', 'guest');
+if (!localStorage.getItem('localChannel'))
+    localStorage.setItem('localChannel', 'General');
 
 document.addEventListener('DOMContentLoaded', () => {
+
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-    document.getElementById("submit").disabled = true;
-    document.getElementById("messagesDiv").style.visibility = "hidden";
 
     var name = localStorage.getItem('name');
 
+    var globalChannelName = localStorage.getItem('localChannel');
+
+    renderChannel(globalChannelName);
+
+    document.getElementById("nameDiv").style.display = "none";
+
     if(name === "guest"){
-        document.getElementById("submit").disabled = false;
+        document.getElementById("nameDiv").style.display = "block";
     }
 
-    const greeting = `Hello, ${name}!`;
-
-    document.querySelector('#greeting').innerHTML = greeting;
-
-    if (!localStorage.getItem('name'))
-        localStorage.setItem('name', 'guest');
+    document.querySelector('#greeting').innerHTML = `Hello, ${name}!`;
 
     document.querySelector('#nameInput').onsubmit = () => {
 
         const request = new XMLHttpRequest();
 
         const name = document.querySelector('#name').value;
+
+        if(!name){
+            alert("Please enter a name");
+            return
+        }
 
         localStorage.setItem("name", name);
 
@@ -35,10 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const greeting = `Hello, ${name}!`;
 
             document.querySelector('#greeting').innerHTML = greeting;
-
-            document.querySelector('#name').value = "";
         };
-        name = localStorage.getItem('name');
 
         const data = new FormData();
 
@@ -55,6 +58,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const channelName = document.querySelector('#channelName').value;
 
+        if(!channelName){
+            alert("Please enter a valid channel name");
+            return
+        }
+
+        renderChannel(channelName);
 
         request.open('POST', '/newChannel');
 
@@ -80,25 +89,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     };
 
-    document.querySelector('#channels').onchange = function() {
+ document.querySelector('#channels').onchange = function() {
+
+        const request = new XMLHttpRequest();
 
         const channel = this.value;
 
         if(channel === "Select a Channel"){
             alert("please select a channel");
-            return
+            return;
         }
 
-        const request = new XMLHttpRequest();
+        document.getElementById(`#${channel}`).style.display = "none";
+
+        localStorage.setItem('localChannel', channel);
+
+        globalChannelName = channel;
 
         request.open('POST', `/${channel}`);
 
-        const channelName = channel.charAt(0).toUpperCase() + channel.slice(1);
-
         request.onload = () => {
-            document.querySelector('#channelTitle').innerHTML = `${channelName} Channel`;
 
-            document.getElementById("messagesDiv").style.visibility = "visible";
+            document.getElementById(`#${channel}`).style.display = "block";
+
         };
 
         const data = new FormData();
@@ -110,15 +123,68 @@ document.addEventListener('DOMContentLoaded', () => {
         return false;
 
     };
+
+    function renderChannel(channel){
+
+        const channelName = channel;
+
+        if(!channelName){
+            alert("invalid put in renderChannel funct");
+            return;
+        }
+
+        const div = document.createElement('div');
+        div.id = `#${channelName}`;
+
+        const h1 = document.createElement('h1');
+        h1.id = `#${channelName}Title`;
+        h1.innerHTML = `${channelName} Channel`;
+
+        const ul = document.createElement('ul');
+        ul.id = `#${channelName}List`;
+
+        const input = document.createElement('input');
+        input.id = `#${channelName}Input`;
+        input.setAttribute("type", "text");
+        input.setAttribute("placeholder", "Type Your Message");
+
+        const button = document.createElement('button');
+        button.id = `#${channelName}Submit`;
+        button.innerHTML = "Send";
+
+        div.appendChild(h1);
+        div.appendChild(ul);
+        div.appendChild(input);
+        div.appendChild(button);
+
+        document.querySelector('#messageColumn').appendChild(div);
+
+        document.getElementById(`#${channelName}`).style.display = "none";
+    }
+
     socket.on('connect', () => {
-        document.querySelector('#messageSubmit').onclick = () => {
-            const message = document.querySelector('#messageInput').value;
+        document.querySelector(`#${globalChannelName}Submit`).onclick = () => {
+
+            const message = document.querySelector(`#${globalChannelName}Input`).value;
+
+            if(!message){
+                alert("Please enter a message");
+                return;
+            }
+
             socket.emit('submit message', message);
         };
     });
+
     socket.on('display messages', message => {
+
         const li = document.createElement('li');
-        li.innerHTML = `${name} says: ${message}`;
-        document.querySelector('#messages').append(li);
+
+        li.innerHTML = message;
+
+        document.getElementById(`#${globalChannelName}List`).add(li);
+
+        document.querySelector(`#${globalChannelName}Input`).value = "";
+
     });
 });
